fd735d9ffb640b0cd499f1894a1429fc343e82d2
NABEEL-AHMED-JMAIL <nabeel.amd@gmail.com>
2017-11-28 14:14:01 +0500

    first commit

---
+++ src/main/java/com/ballistic/pdftest/PdftestApplication.java
@@ -0,0 +1,12 @@
+package com.ballistic.pdftest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdftestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdftestApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ballistic/pdftest/PdftestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ballistic.pdftest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PdftestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
